078c85d4d60de69f5f9dab66e5463f030ec14614
GermanAizek <GermanAizek@yandex.ru>
2019-02-20 18:31:24 +0300

    [ADD] Added delay and progress bar

--- spamer.py
+++ spamer.py
@@ -6,9 +6,31 @@ import configparser
 import sys
 
 from threading import Thread
+from time import sleep
 
 arrayEmails = []
 threads = []
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
 def mthreadLoginSend(email, msg, username, password):
 	server = smtplib.SMTP(config.get("Settings", "Host") + ': ' + config.get("Settings", "Port"))
@@ -17,6 +39,7 @@ def mthreadLoginSend(email, msg, username, password):
 	server.sendmail(msg['From'], email, msg.as_string())
 	print("Successfully sent email to %s:" % (email))
 	server.quit()
+	sleep(1)
 
 try:
 	config = configparser.ConfigParser()
@@ -52,5 +75,9 @@ for email in arrayEmails:
 	thread.start()
 	threads.append(thread)
 
-for t in threads:
+length = len(threads)
+printProgressBar(0, length, prefix = 'Sending:', suffix = 'Complete', length = 50)
+for idx, t in enumerate(threads):
 	t.join()
+	sleep(1)
+	printProgressBar(idx + 1, length, prefix = 'Sending:', suffix = 'Complete', length = 50)
